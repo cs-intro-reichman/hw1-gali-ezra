c8aecb6f5653147865d8546d63f4eca8b714ad91
galie-ezra <ggaliezra@gmail.com>
2024-11-07 19:25:19 +0200

    another changes

--- Bill3.java
+++ Bill3.java
@@ -16,6 +16,6 @@ public class Bill3 {
 	    int amount = Integer.parseInt(args[3]);
 	    double splitAmount= Math.ceil((amount/3.0));
 
-		System.out.println("Dear " + name3 + ", "+ name2 + " and " + name1 + ": pay " + splitAmount + " Shekels each");
+		System.out.println("Dear " + name3 + ", "+ name2 + ", and " + name1 + ": pay " + splitAmount + " Shekels each");
 	}
 }
--- FVCalc.java
+++ FVCalc.java
@@ -18,7 +18,7 @@ public class FVCalc {
 
 		double futureValue = currentValue * Math.pow((1 + rate), numOfYears);
 
-		System.out.println("After "+ numOfYears + " years, a " + currentValue + "$ saved at " + doubleRate + "% will yield " +(int) futureValue + "$");
+		System.out.println("After "+ numOfYears + " years, a $" + currentValue + " saved at " + doubleRate + "% will yield " +(int) futureValue + "$");
 		
 	}
 }
